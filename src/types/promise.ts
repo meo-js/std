1492951a3c472e9fd86f8b1476a1647ccf1aaa01
SmallMain <smallmain@outlook.com>
2025-03-30 21:39:13 +0800

    feat: enhance type definitions and add array buffer utilities
    - Improved documentation for MaybePromise and MaybePromiseLike types
    - Added utility functions for ArrayBuffer and TypedArray checks

--- src/types/promise.ts
+++ src/types/promise.ts
@@ -1,31 +1,37 @@
 /**
- * 描述类型可能为 `Promise` 类型
+ * 可能是 {@link Promise} 的类型
  *
- * @param T 任意类型
+ * @template T 兑现值类型
  */
 export type MaybePromise<T = unknown> = T | Promise<T>;
 
 /**
- * 描述类型可能为 `PromiseLike` 类型
+ * 可能是 {@link PromiseLike} 的类型
  *
- * @param T 任意类型
+ * @template T 兑现值类型
  */
 export type MaybePromiseLike<T = unknown> = T | PromiseLike<T>;
 
 /**
- * 描述 {@link Promise} 类型的值类型
+ * 返回 {@link Promise} 兑现后的类型
+ *
+ * @template T {@link Promise} 类型
  */
-export type PromiseValue<T extends Promise<unknown>> =
+export type AwaitedOnce<T extends Promise<unknown>> =
     T extends Promise<infer U> ? U : T;
 
 /**
- * 描述 {@link PromiseLike} 类型的值类型
+ * 返回 {@link PromiseLike} 兑现后的类型
+ *
+ * @template T {@link PromiseLike} 类型
  */
-export type PromiseLikeValue<T extends PromiseLike<unknown>> =
+export type AwaitedLikeOnce<T extends PromiseLike<unknown>> =
     T extends PromiseLike<infer U> ? U : T;
 
 /**
- * {@link Promise.withResolvers} 方法的返回值
+ * {@link Promise.withResolvers} 方法返回值
+ *
+ * @template T 兑现值类型
  */
 export type PromiseResolvers<T = unknown> = {
     promise: Promise<T>;
--- src/utils/guard.ts
+++ src/utils/guard.ts
@@ -222,3 +222,129 @@ export function isPromise(value: unknown): value is Promise<unknown> {
 export function isNativeCode(target: Function): boolean {
     return Function.prototype.toString.call(target).includes("native code");
 }
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link ArrayBuffer}
+ */
+export function isArrayBuffer(value: unknown): value is ArrayBuffer {
+    return value instanceof ArrayBuffer;
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link ArrayBufferView}
+ */
+export function isArrayBufferView(value: unknown): value is ArrayBufferView {
+    return ArrayBuffer.isView(value);
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link TypedArray} 类型之一
+ */
+export function isTypedArray(value: unknown): value is TypedArray {
+    return (
+        isAnyObject(value)
+        && ArrayBuffer.isView(value)
+        && !(value instanceof DataView)
+    );
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link DataView}
+ */
+export function isDataView(value: unknown): value is DataView {
+    return value instanceof DataView;
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link Int8Array}
+ */
+export function isInt8Array(value: unknown): value is Int8Array {
+    return value instanceof Int8Array;
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link Uint8Array}
+ */
+export function isUint8Array(value: unknown): value is Uint8Array {
+    return value instanceof Uint8Array;
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link Uint8ClampedArray}
+ */
+export function isUint8ClampedArray(
+    value: unknown,
+): value is Uint8ClampedArray {
+    return value instanceof Uint8ClampedArray;
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link Int16Array}
+ */
+export function isInt16Array(value: unknown): value is Int16Array {
+    return value instanceof Int16Array;
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link Uint16Array}
+ */
+export function isUint16Array(value: unknown): value is Uint16Array {
+    return value instanceof Uint16Array;
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link Int32Array}
+ */
+export function isInt32Array(value: unknown): value is Int32Array {
+    return value instanceof Int32Array;
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link Uint32Array}
+ */
+export function isUint32Array(value: unknown): value is Uint32Array {
+    return value instanceof Uint32Array;
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link Float32Array}
+ */
+export function isFloat32Array(value: unknown): value is Float32Array {
+    return value instanceof Float32Array;
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link Float64Array}
+ */
+export function isFloat64Array(value: unknown): value is Float64Array {
+    return value instanceof Float64Array;
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link BigInt64Array}
+ */
+export function isBigInt64Array(value: unknown): value is BigInt64Array {
+    return value instanceof BigInt64Array;
+}
+
+/**
+ * @param value 任意值
+ * @returns 返回值是否为 {@link BigUint64Array}
+ */
+export function isBigUint64Array(value: unknown): value is BigUint64Array {
+    return value instanceof BigUint64Array;
+}
