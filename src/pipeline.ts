0790b0715b1d7c00c0db0431fdbff18995b3d3f7
SmallMain <smallmain@outlook.com>
2025-06-04 00:04:06 +0800

    feat: pipeline

---
+++ src/pipeline.ts
@@ -0,0 +1,28 @@
+// import { isArray, isString, isTypedArray } from "./predicate.js";
+
+// export function pipeline<T1, T2, T3>(a: T1, b: T2, c: T3) {
+//     function handle() {}
+
+//     return (input: Iterable<unknown>) => {
+//         // string fast path
+//         if (isString(input)) {
+
+//             // array fast path
+//         } else if (isArray(input) || isTypedArray(input)) {
+
+//         } {
+//             for (const element of input) {
+//             }
+//         }
+//     };
+// }
+
+// type Pipe = (input: any) => any;
+
+// export function uncompose(str: string): number {
+//     return str.charCodeAt(0);
+// }
+
+// // readable.pipe
+
+// pipeline(uncompose, null!, null!)("hello world");
